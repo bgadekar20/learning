e45b6de83321aaef730f4a94e9e7310001507b0a
bhagya <bgadekar@asu.edu>
2017-08-20 15:48:45 -0700

    status 2

--- contact list/public/controllers/controller.js
+++ contact list/public/controllers/controller.js
@@ -1,3 +1,25 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) 
-		{ console.log("Hello World from controller"); }]);
+function AppCtrl($scope) {
+	console.log("Hey from controller");
+
+
+p1 = {
+	name: 'Abc',
+	email: 'xyz',
+	number: '111111111'
+};
+
+p2 = {
+	name: 'def',
+	email: 'xyz',
+	number: '111111111'
+};
+p3 = {
+	name: 'ghi',
+	email: 'xyz',
+	number: '111111111'
+};
+
+var contactList = [p1,p2,p3];
+$scope.contactList=contactList;
+
+};
--- contact list/server.js
+++ contact list/server.js
@@ -4,4 +4,4 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.listen(3000);
-console.log("SErver runningon 3000");
+console.log("Server running on 3000");
